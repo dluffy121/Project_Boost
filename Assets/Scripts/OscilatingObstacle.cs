94914547c38aebffd78b6b343f74e284e2c1bc6a
dluffy121 <waghela.sonic@gmail.com>
2019-08-29 17:09:27 +0530

    Added 3rd level and Debug Mode

---
+++ Assets/Scripts/OscilatingObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscilatingObstacle : MonoBehaviour
+{
+    //This script ulike the MovingObstacle uses SIN wave function for oscilation
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;//Total oscillation time
+
+    const float tau = Mathf.PI * 2;
+    float movementFactor;
+    float cycles;
+    float rawSinWave;
+
+    Vector3 startingPos;
+
+    void Start()
+    {
+        startingPos = transform.position;    
+    }
+
+    void Update()
+    {
+        //will give weird bug when period set to 0 since floats cannot be compared due to massive decimal places
+        if (period <= Mathf.Epsilon) { return; }    //disables oscilation since Epsilion is smallest float no. | can also use Floor to floor float to 0 but use ==
+        cycles = Time.time / period;
+        rawSinWave = Mathf.Sin(cycles * tau);       //It generates sin wave  bracket value
+
+        movementFactor = rawSinWave / 2f + 0.5f;    //this will determine how much distance is left to complete 1 oscilation for sin wave
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Assets/Scripts/RocketTracker.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketTracker : MonoBehaviour
+{
+    public static class Constants
+    {
+        public static readonly Vector3 offset = new Vector3(0.0f, 1f, 0.0f);
+    }
+
+    public Transform lookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+    public Vector3 offset= new Vector3(0.0f,1f,0.0f);
+
+    const float speed = 0.5f;
+
+    float yangle = 0.0f;
+    float xangle = 0.0f;
+    float xdistance = 0.0f;
+    float zdistance = 15f;
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        KeyUpCameraMovement();
+        KeyPressCameraMovement();
+    }
+
+    private void KeyUpCameraMovement()
+    {
+        if(!Input.GetKey(KeyCode.Space))
+        {
+            if (zdistance >= 15f)
+                zdistance -= 0.05f;
+        }
+        if (!Input.GetKey(KeyCode.D))
+        {
+            if (xdistance >= 0f)
+                xdistance -= 0.05f * speed;
+            if (yangle >= 0f && xangle<=0f)
+            {
+                yangle -= 0.5f;
+                xangle += 0.5f;
+            }
+        }
+        if (!Input.GetKey(KeyCode.A))
+        {
+            if (xdistance <= 0f)
+                xdistance += 0.05f * speed;
+            if (yangle <= 0f && xangle <=0f)
+            {
+                yangle += 0.5f;
+                xangle += 0.5f;
+            }
+        }
+    }
+    private void KeyPressCameraMovement()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (zdistance <= 17f)
+                zdistance += 0.1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (xdistance <= 3f)
+                xdistance += 0.1f;
+            while (yangle <= 5f && xangle >= 5f)
+            {
+                yangle += 0.1f;
+                xangle -= 0.1f;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (xdistance >= -3f)
+                xdistance -= 0.1f;
+            while (yangle >= 5f && xangle >=5f)
+            {
+                yangle -= 0.1f;
+                xangle -= 0.1f;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        CameraMovement();
+    }
+
+    private void CameraMovement()
+    {
+        Vector3 dir = new Vector3(xdistance, 0, -zdistance);
+        Quaternion rotation = Quaternion.Euler(xangle, yangle, -xangle);
+        camTransform.position = lookAt.position + Constants.offset + rotation * dir;
+    }
+}
